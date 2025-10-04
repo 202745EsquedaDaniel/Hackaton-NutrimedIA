8af891235de7fc1e2f8a29d37ccc1e70cf1aa189
Eduardo esqueda <dani@static-195-206-189-54.alestra.net.mx>
2025-10-04 09:34:14 -0600

    [ADD] - drawer added

--- lib/features/auth/home/presentation/pages/home_page.dart
+++ lib/features/auth/home/presentation/pages/home_page.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nutrimedai/features/auth/home/presentation/components/my_drawer.dart';
+import 'package:nutrimedai/features/auth/presentation/cubits/auth_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = context.watch<AuthCubit>().currentUser;
+
     return Scaffold(
       appBar: AppBar(title: Text('Home Page'), centerTitle: true),
+      drawer: const MyDrawer(),
       body: Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (user != null) ...[
+              Text(
+                'Hola, ${user.name}',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(user.email, style: const TextStyle(fontSize: 16)),
+            ] else ...[
+              const Text(
+                'Welcome to the Home Page!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Action when the button is pressed
           ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Floating Action Button Pressed')),
+            const SnackBar(content: Text('Floating Action Button Pressed')),
           );
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
